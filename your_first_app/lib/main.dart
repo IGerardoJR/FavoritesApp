1bc68bcb2f4bfba32db18dcc7f75c760baeb53b5
Isaias <igerardo0808@gmail.com>
2024-02-01 18:00:56 -0600

    barra lateral

--- your_first_app/lib/main.dart
+++ your_first_app/lib/main.dart
@@ -8,11 +8,7 @@ void main(){
 
 
 class MyApp extends StatelessWidget{
-
- 
-
   MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
 
@@ -56,76 +52,110 @@ class MyAppState extends ChangeNotifier{
 
 
 
-class MyHomePage extends StatelessWidget{
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  late double _deviceWidth;
-
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-        _deviceWidth = MediaQuery.of(context).size.width;
-    // TODO: implement build
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-   
-  IconData icon;
 
-  if(appState.favorites.contains(pair)){
-    icon = Icons.favorite;
-  }else{
-    icon = Icons.favorite_border;
-  }
-    return Scaffold(
-      body:Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        
-        children: [
-          Text('A Random AWESOME Idea'),
-          BigCard(pair: pair),
-          
-          Container(
-            margin: EdgeInsets.all(0.5),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // Boton Like
-                ElevatedButton(
-              onPressed: () {
-                appState.toggleFavorite();
-              } ,
-              child:Row(
-                children: [
-                  Icon(
-                    icon,
-                  ),
-                  Text('Like')
-                ],
-              ),
-              ),
-            
-            SizedBox(
-              width: _deviceWidth * 0.02,
-            ),
-              // Boton Next
-              ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              } ,
-              child:Text(
-                'Next'
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+
+      default:
+          throw UnimplementedError('No widget for $selectedIndex');
+    }
+    return LayoutBuilder(
+      builder: (context,constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: false,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
                 ),
-              )
-              ],
-            ),
-          )
-        ],
-        
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
                 ),
-      )
-      
+              ),
+            ],
+          ),
+        );
+      }
     );
   }
-  
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class BigCard extends StatelessWidget {
